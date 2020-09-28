325f99a72ffdc4f03466a65e2721b5383d162657
Vitor Sandes <sandesvitor@gmail.com>
2020-09-28 13:00:19 -0300

    Change port organization for deployment on Heroku

--- src/app.js
+++ src/app.js
@@ -3,12 +3,9 @@ const routes = require('./routes')
 
 require('dotenv').config()
 require('./db/database/index')
-const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
 app.use(routes)
 
-app.listen(PORT, () => {
-    console.log('Server listenning on Port [%s]', PORT)
-})
+app.listen(process.env.PORT || 5000)
