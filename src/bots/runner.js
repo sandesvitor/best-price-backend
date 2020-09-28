02c019d977a166aaef0a3eac7ba05844e8519ca6
Vitor Sandes <sandesvitor@gmail.com>
2020-09-28 19:11:59 -0300

    Starting db storage

--- src/bots/runner.js
+++ src/bots/runner.js
@@ -6,12 +6,11 @@ require('dotenv').config()
 require('../db/database/index')
 
 const runBots = async () => {
-    // await amazon()
-    // await kabum()
-    console.log(Math.random * 100)
+    await amazon()
+    await kabum()
 }
 
-nodeCron.schedule("*/10 */1 * * *", async () => {
+nodeCron.schedule("*/20 */1 * * *", async () => {
     console.log("START OF WEB SCRAPPING at: ", new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''))
     await runBots()
     console.log("END OF WEB SCRAPPING at: ", new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''))
