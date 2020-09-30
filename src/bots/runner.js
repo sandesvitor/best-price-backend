9870f65e5a2bad707c32c021436937f718af0674
Vitor Sandes <sandesvitor@gmail.com>
2020-09-30 00:12:54 -0300

    Removed gulp

--- src/bots/runner.js
+++ src/bots/runner.js
@@ -5,14 +5,13 @@ const nodeCron = require('node-cron')
 require('dotenv').config()
 require('../db/database/index')
 
-const runner = async () => {
-    await amazon()
-    await kabum()
-}
-
-// nodeCron.schedule("0 4 * * *", async () => {
+// const runner = async () => {
 //     await amazon()
 //     await kabum()
-// })
+// }
+// runner()
 
-runner()
+nodeCron.schedule("0 4 * * *", async () => {
+    await amazon()
+    await kabum()
+})
